9a9ea4f386d642c039bfcd53059e7bd6e10805e8
devel-maverick <codemaverick00@gmail.com>
2026-01-25 04:13:21 +0530

    Fix wildcard route for Express v5

--- Backend/server.js
+++ Backend/server.js
@@ -40,9 +40,10 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
 
 
 const PORT = 3000;
