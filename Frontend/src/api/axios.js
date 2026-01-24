fffcdc79981d40fde2e5536dbe43965433d8f13b
devel-maverick <codemaverick00@gmail.com>
2026-01-25 03:47:34 +0530

    final deploy ready

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,9 +1,8 @@
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000/api"
-    : "/api";
+import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+export default axiosInstance;
